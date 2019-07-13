8ff838f269fab01692ae936ef25e54a6d587741b
Nicolas Yepes <nicolasyepespulido@gmail.com>
2019-07-13 16:12:53 -0500

    working on speed round

--- Trivia/SpeedQuestionViewController.swift
+++ Trivia/SpeedQuestionViewController.swift
@@ -17,22 +17,47 @@ class SpeedQuestionViewController: UIViewController {
     @IBOutlet weak var fourthAnswerLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var timeRemainingLabel: UILabel!
+    var questions = [String: String]()
+    var score = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let url = URL(string: getRandomQuestion()) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                    parse(json: json)
+                    return
+                
+            }
+        }
+//        loadError()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getRandomQuestion () -> String {
+        
+        var categories = [String]()
+        categories.append("https://opentdb.com/api.php?amount=25&category=21&difficulty=medium&type=multiple")
+        categories.append( "https://opentdb.com/api.php?amount=30&category=23&difficulty=medium&type=multiple")
+        return categories.randomElement()!
+//        let category = categories.randomElement()!
+//        let result = json ["results"]["question"]
+//        print(result)
+//        let question = result! ["question"]
+        
     }
-    */
-
+//    
+//    func parse(json: JSON) {
+//        for result in json["results"].arrayValue {
+//            let question = result["question"].stringValue
+//            let answer = result["correct_answer"].stringValue
+//            let wrongAnswer = result["wrong_answer"]
+//            //print(question)
+//            print(answer)
+//        }
+////        let random = Int.random(in: 1...5)
+////        let question = json["results"].randomElement()
+////        questionLabel.text = question!["question"] as! String
+////        let result = question["question"]!
+//
+//    }
 }
