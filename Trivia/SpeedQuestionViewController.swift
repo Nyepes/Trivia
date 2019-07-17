c02b1f21a1e77ee7837b3a6bf7e17eccd99c2e2e
Allan Zhang <allanwzhang04@gmail.com>
2019-07-16 20:36:01 -0500

    Made user experience better, fixed bugs

--- Trivia/SpeedQuestionViewController.swift
+++ Trivia/SpeedQuestionViewController.swift
@@ -64,7 +64,7 @@ class SpeedQuestionViewController: UIViewController {
                 self.ended = true
                 t.invalidate()
                 self.timeRemainingLabel.text = "You Lose"
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     self.questionLabel.text = "Score: " + String(self.scores.currentScore)
                     self.firstAnswerLabel.text = ""
                     self.secondAnswerLabel.text = ""
@@ -137,7 +137,7 @@ class SpeedQuestionViewController: UIViewController {
                             streakLabel.text = "Streak: \(scores.streak)"
                             self.wait = true
                             saveData()
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                 self.updateLabels()
                                 for label in self.labelsArray {
                                     label.backgroundColor = .white
@@ -154,7 +154,7 @@ class SpeedQuestionViewController: UIViewController {
                             self.wait = true
                             saveData()
                             scores.streak = 0
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                 for label in self.labelsArray {
                                     label.backgroundColor = .white
                                     self.updateLabels()
