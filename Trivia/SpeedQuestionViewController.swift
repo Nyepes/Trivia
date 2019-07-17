a813593764c299ce9021fd16b1857c092847bdcd
Alvin Tuo <atuo@wyomingseminary.org>
2019-07-16 21:36:48 -0500

    Added backgrounds

--- Trivia/SpeedQuestionViewController.swift
+++ Trivia/SpeedQuestionViewController.swift
@@ -140,7 +140,7 @@ class SpeedQuestionViewController: UIViewController {
                             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                 self.updateLabels()
                                 for label in self.labelsArray {
-                                    label.backgroundColor = .white
+                                    label.backgroundColor = .clear
                                 }
                                 self.wait = false
                             }
@@ -156,7 +156,7 @@ class SpeedQuestionViewController: UIViewController {
                             scores.streak = 0
                             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                 for label in self.labelsArray {
-                                    label.backgroundColor = .white
+                                    label.backgroundColor = .clear
                                     self.updateLabels()
                                 }
                                 self.wait = false
