9d09d46b8e3cf14b9454328c890d2e2537890326
Allan Zhang <allanwzhang04@gmail.com>
2019-07-18 09:56:53 -0500

    lines break?

--- Trivia/ActualQuestionViewController.swift
+++ Trivia/ActualQuestionViewController.swift
@@ -22,12 +22,14 @@ class ActualQuestionViewController: UIViewController {
     var labelsArray = [UILabel]()
     
     override func viewDidLoad() {
-        for label in labelsArray {
-            label.lineBreakMode = .byWordWrapping
-        }
         loadData()
         questionArray.shuffle()
         labelsArray = [answer1Label, answer2Label, answer3Label, answer4Label]
+        questionLabel.lineBreakMode = .byWordWrapping
+        for label in labelsArray {
+            label.lineBreakMode = .byWordWrapping
+            label.numberOfLines = 0
+        }
         super.viewDidLoad()
         count[genreNum] = 0
         totalCount[genreNum] = 0
@@ -82,7 +84,7 @@ class ActualQuestionViewController: UIViewController {
     }
     
     func parse(json: JSON) {
-        for i in 0...14 {
+        for i in 0...17 {
             let result = json["results"][i]
             let question = result["question"].stringValue.stringByDecodingHTMLEntities
             let correct = result["correct_answer"].stringValue.stringByDecodingHTMLEntities
@@ -129,9 +131,9 @@ class ActualQuestionViewController: UIViewController {
         }
         if numOfQuestions < 18 {//fix?
             let selectedPoint = sender.location(in: view)
-            numOfQuestions += 1
             for label in labelsArray {
                 if(label.frame.contains(selectedPoint)) {
+                    numOfQuestions += 1
                     totalCount[genreNum] += 1
                     self.wait = true
                     if label.text! == questionArray[0]["correct"] {
@@ -195,30 +197,15 @@ private let characterEntities : [ Substring : Character ] = [
 ]
 
 extension String {
-    
-    /// Returns a new string made by replacing in the `String`
-    /// all HTML character entity references with the corresponding
-    /// character.
+
     var stringByDecodingHTMLEntities : String {
         
-        // ===== Utility functions =====
-        
-        // Convert the number in the string to the corresponding
-        // Unicode character, e.g.
-        //    decodeNumeric("64", 10)   --> "@"
-        //    decodeNumeric("20ac", 16) --> "€"
         func decodeNumeric(_ string : Substring, base : Int) -> Character? {
             guard let code = UInt32(string, radix: base),
                 let uniScalar = UnicodeScalar(code) else { return nil }
             return Character(uniScalar)
         }
-        
-        // Decode the HTML character entity to the corresponding
-        // Unicode character, return `nil` for invalid input.
-        //     decode("&#64;")    --> "@"
-        //     decode("&#x20ac;") --> "€"
-        //     decode("&lt;")     --> "<"
-        //     decode("&foo;")    --> nil
+
         func decode(_ entity : Substring) -> Character? {
             
             if entity.hasPrefix("&#x") || entity.hasPrefix("&#X") {
@@ -229,34 +216,25 @@ extension String {
                 return characterEntities[entity]
             }
         }
-        
-        // ===== Method starts here =====
-        
+  
         var result = ""
         var position = startIndex
-        
-        // Find the next '&' and copy the characters preceding it to `result`:
+
         while let ampRange = self[position...].range(of: "&") {
             result.append(contentsOf: self[position ..< ampRange.lowerBound])
             position = ampRange.lowerBound
-            
-            // Find the next ';' and copy everything from '&' to ';' into `entity`
             guard let semiRange = self[position...].range(of: ";") else {
-                // No matching ';'.
                 break
             }
             let entity = self[position ..< semiRange.upperBound]
             position = semiRange.upperBound
             
             if let decoded = decode(entity) {
-                // Replace by decoded character:
                 result.append(decoded)
             } else {
-                // Invalid entity, copy verbatim:
                 result.append(contentsOf: entity)
             }
         }
-        // Copy remaining characters to `result`:
         result.append(contentsOf: self[position...])
         return result
     }
